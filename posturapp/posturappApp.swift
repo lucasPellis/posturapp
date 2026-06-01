8158cd41dfaa1424c1b7449763a0048883554693
Lucas Pellis <lucaspellis@hotmail.com>
2026-06-01 19:28:09 +0200

    Initial Commit

---
+++ posturapp/posturappApp.swift
@@ -0,0 +1,17 @@
+//
+//  posturappApp.swift
+//  posturapp
+//
+//  Created by Lucas Pellis on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct posturappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
